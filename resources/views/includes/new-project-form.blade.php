799bb37c6912eb97dcd9feb99f4c43abf744bcc4
Minh Duc Nguyen <dekarvn@gmail.com>
2016-07-24 14:50:41 +1000

    Updated Project creation validation

--- resources/views/includes/new-project-form.blade.php
+++ resources/views/includes/new-project-form.blade.php
@@ -1,15 +1,17 @@
 <form action="{{ url('projects') }}" method="post" role="form">
-	<legend>Form Title</legend>
 
     <input type="hidden" name="_token" value="{{ csrf_token() }}">
-	<div class="form-group">
+
+	<div class="form-group {{ $errors->has('title') ? "has-error" : "" }}">
 		<label for="title" class="col-sm-2">Title</label>
-		<input type="text" class="form-control" name="title" id="" placeholder="Project Title...">
+		<input type="text" class="form-control" name="title" placeholder="Project Title" required>
+        {!! $errors->first('title', '<span class="help-block">:message</span>') !!}
 	</div>
 
-    <div class="form-group">
-        <label for="description" class="col-sm-2">Description</label>
-        <textarea name="description" class="form-control" placeholder="Project Description"></textarea>
+    <div class="form-group {{ $errors->has('description') ? "has-error" : "" }}">
+        <label for="description">Description</label>
+        <textarea rows="15" name="description" class="form-control" placeholder="Project Description" required></textarea>
+        {!! $errors->first('description', '<span class="help-block">:message</span>') !!}
     </div>
 
 	<button type="submit" class="btn btn-primary">Submit</button>
